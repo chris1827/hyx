ce981df02c2c78c8df91177e5b1eda4c9aea6a88
chris1827 <chris1827@163.com>
2012-10-09 11:00:11 +0800

    second commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ *  @description:entry file index.php
+ *  @date:2012-7-6 Friday
+ *  @author:chris.yahn
+ */
+	require_once('./config/config.php');
+    require_once(CYMVC.'/core.php');
+	new core();
+?>
